ea2107f0fb849ce241652817e4cc57d8b555566a
Marco Tramarin <marco.tramarin@inventis.it>
2019-12-09 09:35:48 +0100

    Introdotti colori ufficiali

--- FantaAsta/Utilities/Constants.cs
+++ FantaAsta/Utilities/Constants.cs
@@ -13,26 +13,26 @@ namespace FantaAsta.Utilities
 		public static string DATA_DIRECTORY_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "FantaAstaManager");
 		public static string DATA_FILE_PATH = Path.Combine(DATA_DIRECTORY_PATH, "FantaAstaData.xml");
 
-		public static Squadra ATALANTA = new Squadra("Atalanta", new SolidColorBrush(Colors.Black), new SolidColorBrush(Colors.Blue));
-		public static Squadra BOLOGNA = new Squadra("Bologna", new SolidColorBrush(Colors.Red), new SolidColorBrush(Colors.Blue));
-		public static Squadra BRESCIA = new Squadra("Brescia", new SolidColorBrush(Colors.Cyan), new SolidColorBrush(Colors.White));
-		public static Squadra CAGLIARI = new Squadra("Cagliari", new SolidColorBrush(Colors.DarkRed), new SolidColorBrush(Colors.Blue));
-		public static Squadra FIORENTINA = new Squadra("Fiorentina", new SolidColorBrush(Colors.Purple), new SolidColorBrush(Colors.White));
-		public static Squadra GENOA = new Squadra("Genoa", new SolidColorBrush(Colors.DarkBlue), new SolidColorBrush(Colors.Red));
-		public static Squadra INTER = new Squadra("Inter", new SolidColorBrush(Colors.Blue), new SolidColorBrush(Colors.Black));
-		public static Squadra JUVENTUS = new Squadra("Juventus", new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Black));
-		public static Squadra LAZIO = new Squadra("Lazio", new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Cyan));
-		public static Squadra LECCE = new Squadra("Lecce", new SolidColorBrush(Colors.Yellow), new SolidColorBrush(Colors.Red));
-		public static Squadra MILAN = new Squadra("Milan", new SolidColorBrush(Colors.Red), new SolidColorBrush(Colors.Black));
-		public static Squadra NAPOLI = new Squadra("Napoli", new SolidColorBrush(Colors.DarkCyan), new SolidColorBrush(Colors.White));
-		public static Squadra PARMA = new Squadra("Parma", new SolidColorBrush(Colors.Yellow), new SolidColorBrush(Colors.Blue));
-		public static Squadra ROMA = new Squadra("Roma", new SolidColorBrush(Colors.Brown), new SolidColorBrush(Colors.Yellow));
-		public static Squadra SAMPDORIA = new Squadra("Sampdoria", new SolidColorBrush(Colors.DarkBlue), new SolidColorBrush(Colors.White));
-		public static Squadra SASSUOLO = new Squadra("Sassuolo", new SolidColorBrush(Colors.Green), new SolidColorBrush(Colors.Black));
-		public static Squadra SPAL = new Squadra("Spal", new SolidColorBrush(Colors.Cyan), new SolidColorBrush(Colors.White));
-		public static Squadra TORINO = new Squadra("Torino", new SolidColorBrush(Colors.Brown), new SolidColorBrush(Colors.White));
-		public static Squadra UDINESE = new Squadra("Udinese", new SolidColorBrush(Colors.Black), new SolidColorBrush(Colors.White));
-		public static Squadra VERONA = new Squadra("Verona", new SolidColorBrush(Colors.SlateBlue), new SolidColorBrush(Colors.Yellow));
+		public static Squadra ATALANTA = new Squadra("Atalanta", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1E71B8")));
+		public static Squadra BOLOGNA = new Squadra("Bologna", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#A21C26")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1A2F48")));
+		public static Squadra BRESCIA = new Squadra("Brescia", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#004E8E")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra CAGLIARI = new Squadra("Cagliari", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AD002A")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#002350")));
+		public static Squadra FIORENTINA = new Squadra("Fiorentina", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#482E92")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra GENOA = new Squadra("Genoa", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00265D")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E1393D")));
+		public static Squadra INTER = new Squadra("Inter", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0068A8")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#221F20")));
+		public static Squadra JUVENTUS = new Squadra("Juventus", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000")));
+		public static Squadra LAZIO = new Squadra("Lazio", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6397D0")));
+		public static Squadra LECCE = new Squadra("Lecce", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF200")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ED1B23")));
+		public static Squadra MILAN = new Squadra("Milan", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB090B")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000")));
+		public static Squadra NAPOLI = new Squadra("Napoli", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#12A0D7")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra PARMA = new Squadra("Parma", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FEC532")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#035183")));
+		public static Squadra ROMA = new Squadra("Roma", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8E1F2F")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F0BC42")));
+		public static Squadra SAMPDORIA = new Squadra("Sampdoria", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0152BC")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra SASSUOLO = new Squadra("Sassuolo", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#21633B")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000")));
+		public static Squadra SPAL = new Squadra("Spal", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1199CC")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra TORINO = new Squadra("Torino", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7B1C20")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")));
+		public static Squadra UDINESE = new Squadra("Udinese", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000")));
+		public static Squadra VERONA = new Squadra("Verona", new SolidColorBrush((Color)ColorConverter.ConvertFromString("#004A9A")), new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FEE500")));
 
 		public static List<Squadra> SERIE_A = new List<Squadra>
 		{
