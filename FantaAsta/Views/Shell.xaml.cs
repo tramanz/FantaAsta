82af18fca34e5aef6ac9a28cf7b8449d6ad6916b
Marco Tramarin <marco.tramarin@inventis.it>
2019-12-13 11:14:51 +0100

    implementata finestra custom

--- FantaAsta/Views/Shell.xaml.cs
+++ FantaAsta/Views/Shell.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using FantaAsta.Enums;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace FantaAsta.Views
 {
@@ -10,6 +15,68 @@ namespace FantaAsta.Views
 		public Shell()
 		{
 			InitializeComponent();
+
+			CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, CloseWindow, CanCloseWindow));
+			CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, MaximizeWindow, CanMaximizeWindow));
+			CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, MinimizeWindow, CanMinimizeWindow));
 		}
+
+		#region Private methods
+
+		private void CloseWindow(object target, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.CloseWindow(this);
+		}
+		private void CanCloseWindow(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void MaximizeWindow(object target, ExecutedRoutedEventArgs e)
+		{
+			ChangeWindowSize();
+			e.Handled = true;
+		}
+		private void CanMaximizeWindow(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void MinimizeWindow(object target, ExecutedRoutedEventArgs e)
+		{
+			SystemCommands.MinimizeWindow(this);
+			e.Handled = true;
+		}
+		private void CanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void OnTitleBarClicked(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+			{
+				ChangeWindowSize();
+			}
+			else
+			{
+				DragMove();
+			}
+		}
+
+		private void ChangeWindowSize()
+		{
+			switch (WindowState)
+			{
+				case WindowState.Maximized:
+					WindowState = WindowState.Normal;
+					break;
+				case WindowState.Normal:
+					WindowState = WindowState.Maximized;
+					break;
+			}
+		}
+
+		#endregion
 	}
 }
