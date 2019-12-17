3b533044f926cd4e0e00bc586cb3848b11221fde
Marco Tramarin <marco.tramarin@inventis.it>
2019-12-17 10:20:29 +0100

    Aggiunto supporto per installer e icona

---
+++ FantaAsta/Installer/Setup_FantaAstaManager.iss
@@ -0,0 +1,47 @@
+[Setup]
+#define ApplicationVersion GetFileVersion('Skeleton\FantaLegaManager.exe')
+AppCopyright=2019 - Marco Tramarin
+AppName=Fanta Lega Manager
+AppVerName=Fanta Lega Manager
+AppVersion={#ApplicationVersion}
+WizardImageStretch=false
+WindowResizable=false
+WindowVisible=true
+BackColor2=clWhite
+SetupIconFile=FantaLegaManager.ico
+RestartIfNeededByRun=false
+DefaultDirName=C:\Program Files (x86)\FantaLegaManager
+AllowRootDirectory=true
+DirExistsWarning=yes
+AllowUNCPath=false
+OutputDir=.
+OutputBaseFilename=FantaLegaManager_{#ApplicationVersion}_setup
+SetupLogging=true
+AppID={{4CF56F15-BA25-4179-BC78-F15231399B3A}
+SolidCompression=true
+ShowLanguageDialog=no
+LanguageDetectionMethod=locale
+DefaultGroupName=Marco Tramarin
+VersionInfoCompany=Marco Tramarin
+AppPublisher=Marco Tramarin
+UninstallDisplayIcon={app}\FantaLegaManager.exe
+
+[Files]
+Source: Skeleton\FantaLegaManager.exe; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\FantaLegaManager.exe.config; DestDir: {app}; Flags: ignoreversion onlyifdoesntexist
+Source: Skeleton\CommonServiceLocator.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\Prism.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\Prism.Unity.Wpf.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\Prism.Wpf.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\System.Runtime.CompilerServices.Unsafe.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\System.Threading.Tasks.Extensions.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\System.ValueTuple.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\System.Windows.Interactivity.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\Unity.Abstractions.dll; DestDir: {app}; Flags: ignoreversion
+Source: Skeleton\Unity.Container.dll; DestDir: {app}; Flags: ignoreversion
+
+[UninstallDelete]
+Type: filesandordirs; Name: "C:\ProgramData\FantaLegaManager"
+    
+[Icons]
+Name: "{commondesktop}\Fanta Lega Manager"; Filename: {app}\FantaLegaManager.exe;
--- FantaAsta/Properties/AssemblyInfo.cs
+++ FantaAsta/Properties/AssemblyInfo.cs
@@ -1,43 +1,34 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Le informazioni generali relative a un assembly sono controllate dal seguente 
-// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
-// associate a un assembly.
-[assembly: AssemblyTitle("FantaAsta")]
+// Le informazioni generali relative a un assembly sono controllate dal seguente set di attributi.
+// Modificare i valori di questi attributi per modificare le informazioni associate a un assembly.
+[assembly: AssemblyTitle("FantaLegaManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FantaAsta")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Marco Tramarin")]
+[assembly: AssemblyProduct("FantaLegaManager")]
+[assembly: AssemblyCopyright("Copyright © 2019 Marco Tramarin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
-// COM, impostare su true l'attributo ComVisible per tale tipo.
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili ai componenti COM.
+// Se è necessario accedere a un tipo in questo assembly da COM, impostare su true l'attributo ComVisible per tale tipo.
 [assembly: ComVisible(false)]
 
-//Per iniziare a creare applicazioni localizzabili, impostare
-//<UICulture>CultureYouAreCodingWith</UICulture> nel file .csproj
-//all'interno di un <PropertyGroup>.  Ad esempio, se si utilizza l'inglese (Stati Uniti)
-//nei file di origine, impostare <UICulture> su en-US.  Rimuovere quindi il commento dall'attributo
-//NeutralResourceLanguage riportato di seguito.  Aggiornare "en-US" nella
-//riga sottostante in modo che corrisponda all'impostazione UICulture nel file di progetto.
+//Per iniziare a creare applicazioni localizzabili, impostare <UICulture>CultureYouAreCodingWith</UICulture> nel file .csproj all'interno di un <PropertyGroup>.
+// Ad esempio, se si utilizza l'inglese (Stati Uniti) nei file di origine, impostare <UICulture> su en-US.
+// Rimuovere quindi il commento dall'attributo NeutralResourceLanguage riportato di seguito.
+// Aggiornare "en-US" nella riga sottostante in modo che corrisponda all'impostazione UICulture nel file di progetto.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //dove si trovano i dizionari delle risorse specifiche del tema
-                                     //(da usare se nella pagina non viene trovata una risorsa,
-                                     // oppure nei dizionari delle risorse dell'applicazione)
-    ResourceDictionaryLocation.SourceAssembly //dove si trova il dizionario delle risorse generiche
-                                              //(da usare se nella pagina non viene trovata una risorsa,
-                                              // nell'applicazione o nei dizionari delle risorse specifiche del tema)
+    //dove si trovano i dizionari delle risorse specifiche del tema (da usare se nella pagina non viene trovata una risorsa, oppure nei dizionari delle risorse dell'applicazione)
+    ResourceDictionaryLocation.None,
+    //dove si trova il dizionario delle risorse generiche (da usare se nella pagina non viene trovata una risorsa, nell'applicazione o nei dizionari delle risorse specifiche del tema)
+    ResourceDictionaryLocation.SourceAssembly
 )]
 
 
