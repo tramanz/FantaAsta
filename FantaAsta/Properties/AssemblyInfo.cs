1a6dd99e439f544319490cc26a919810ff315ffa
Marco <marco@tramarin@gmail.com>
2019-12-25 11:12:37 +0100

    Aggiornata AssemblyFileVersion

--- FantaAsta/Properties/AssemblyInfo.cs
+++ FantaAsta/Properties/AssemblyInfo.cs
@@ -43,4 +43,4 @@ using System.Windows;
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
